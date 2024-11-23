50c5b79824dd6cffabc1bfce5a6e8a014a30bd3e
Chero14 <franco_idat@hotmail.com>
2024-11-23 07:51:00 -0500

    Primera confirmación del repositorio

---
+++ bussinessdomain/cliente/src/main/java/com/padre/cliente/infrastructure/service/FirebaseService.java
@@ -0,0 +1,13 @@
+package com.padre.cliente.infrastructure.service;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.padre.cliente.infrastructure.entity.Cliente;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FirebaseService {
+
+
+}
+
---
+++ infrastructure/keycloackadapter/src/test/java/com/padre/keycloackadapter/KeycloackadapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padre.keycloackadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloackadapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
